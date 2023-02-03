c7ac07ae1e52cbb230ee20c83f339937ce2b5519
jay_wesker <jiras.adt@gmail.com>
2023-02-03 10:47:17 +0700

    📝 Create PHP

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <!--head เอาไว้สำหรับใส่ข้อมูลต่างๆที่เป็น meta data-->
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <header></header>
+        <div class="container">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni eum quibusdam voluptates suscipit sapiente alias vel consectetur repudiandae dolores? Doloribus quia ab dicta sint corrupti ex dolorum corporis dolores labore.
+            
+            <br>
+            <?php 
+                // Input your code PHP here!!!
+                echo 1+1; 
+            ?>
+            
+            <br>
+            <?php
+                echo 2+3;
+            ?>
+        </div>
+    </body>
+</html>
