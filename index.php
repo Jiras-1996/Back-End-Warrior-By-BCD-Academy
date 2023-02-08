afc4ae8bc2d86d0d8edca09a4055243f088071e1
Albert J. Wesker <jiras.adt@gmail.com>
2023-02-08 21:41:23 +0700

    ✅Update File

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    
+        <link rel="stylesheet" href="app.css">
     </head>
     <body>
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id necessitatibus illum corrupti autem hic nobis, provident quaerat accusantium inventore recusandae voluptate harum iste placeat minus delectus aspernatur omnis dolore quis?
+                </div>
+            </div>
+        </div>
+
+    
         <?php 
         ?>
     </body>
